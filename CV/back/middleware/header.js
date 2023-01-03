e9f61b93c86f413ad989663f6ca7dc080f8914a4
dop <121168490+153dop@users.noreply.github.com>
2023-01-03 14:37:03 +0100

    Add files via upload

---
+++ CV/back/middleware/header.js
@@ -0,0 +1,15 @@
+module.exports = function headers() {
+   return function headers(req, res, next) {
+      const allowedOrigins = process.env.ALLOWED_ORIGINS;
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+      }
+      res.setHeader(
+         'Access-Control-Allow-Headers',
+         'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+      );
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+      next();
+   };
+};
